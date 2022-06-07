b653bc058aa33c9b7ed32c0d492ba2ceb941c42d
Kishorè Shanto <tinykishore@gmail.com>
2022-06-07 14:49:11 +0600

    added raw mode
    Signed-off-by: Kishorè Shanto <tinykishore@gmail.com>

--- src/main.c
+++ src/main.c
@@ -3,8 +3,26 @@
 //
 
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios original;
+
+void disableRawMode() {
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &original);
+}
+
+void enableRawMode() {
+    tcgetattr(STDERR_FILENO, &original);
+    atexit(disableRawMode);
+    struct termios raw = original;
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+    enableRawMode();
+
     int c;
     while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
